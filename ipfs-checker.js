b4dff79f17e340d6221ab975c7ec88abbf25ff6b
MrFasolo97 <79fasolo97@gmail.com>
2022-04-07 12:37:34 +0000

    Fix request module, fix a typo.

--- ipfs-checker.js
+++ ipfs-checker.js
@@ -1,16 +1,16 @@
-import { get } from 'request';
-import { execFile } from 'child_process';
+const request = require('request');
+const child_process = require('child_process');
 const local_url = 'http://localhost:5002'
 
 function checkDelay() {
-  get({
+  request.get({
     uri: (local_url + "/webui"),
     json: true,
     headers: {'User-Agent': 'request'}
     }, (err, response, data) => {
       if (err) {
         restart('ipfs')
-      } else if (response != null && response.statusCode !== 301) {
+      } else if (response != null && response.statusCode !== 200) {
         console.log('Status:', response.statusCode)
         restart('ipfs')
       }
@@ -19,9 +19,9 @@ function checkDelay() {
 
 function restart(service) {
   console.log(`${service} restarting...`)
-  execFile('systemctl', ['restart', service], (error, stdout, stderr) => {
+  child_process.execFile('systemctl', ['restart', service], (error, stdout, stderr) => {
     if (error) throw error
-    console.log(`${service} restarted !`)
+    console.log(`${service} restarted!`)
   })
 }
 setInterval(checkDelay, 3000)
